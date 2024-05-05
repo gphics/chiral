8c72aff742c1e51eb4b30e8227752e004b596ba0
gphics2~ <abdulbasitabdulakeem234@gmail.com2~>
2024-05-05 03:50:38 +0100

    fixing conditionals

--- server/Controllers/passcodeCtrl/main/getPasscode.js
+++ server/Controllers/passcodeCtrl/main/getPasscode.js
@@ -9,18 +9,20 @@ module.exports = async (req, res, next) => {
       return next("id param must be provided");
     }
     const passcode = await passcodeModel.findOne({ key });
-    const project = await projectModel
-      .findById(passcode.projectId)
-      .populate("brief");
-    const { brief: prevBrief } = project;
-    const des = JSON.parse(JSON.stringify(prevBrief));
-    const brief = { ...des, jobType: project.type };
-    // console.log(brief);
-
     if (!passcode) {
       return next(errGen("passcode does not exist", 404));
     }
-    return res.json({ data: { passcode, brief }, err: null });
+    if (passcode.projectId) {
+      const project = await projectModel
+        .findById(passcode.projectId)
+        .populate("brief");
+      const { brief: prevBrief } = project;
+      const des = JSON.parse(JSON.stringify(prevBrief));
+      const brief = { ...des, jobType: project.type };
+      return res.json({ data: { passcode, brief }, err: null });
+    }
+
+    return res.json({ data: { passcode }, err: null });
   } catch (error) {
     return next(errGen(error.message));
   }
